08114cef98fd7fdd519d0ba0c4259950f325c5dc
FryenX <ardiwidana@gmail.com>
2025-05-31 00:24:13 +0800

    adding printInvoice feature(still have major issue)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("org.mindrot:jbcrypt:0.4")
+    implementation("com.github.DantSu:ESCPOS-ThermalPrinter-Android:3.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
